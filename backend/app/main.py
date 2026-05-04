872275f6d9e3a385a9bb4ab91a30c1caa56bc712
Kush Painuly <160495043+Kush-Painuly@users.noreply.github.com>
2026-05-04 21:41:53 +0530

    redeployed

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://review-service-offer.vercel.app/"
+        "https://review-service-offer.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
