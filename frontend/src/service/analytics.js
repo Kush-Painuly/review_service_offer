a79980fd0e910d5cdca3f917aba460e7e44c412c
Kush Painuly <160495043+Kush-Painuly@users.noreply.github.com>
2026-05-10 16:56:29 +0530

    debug env

--- frontend/src/service/analytics.js
+++ frontend/src/service/analytics.js
@@ -1,26 +1,16 @@
 import axios from "axios";
 
 const API_BASE = import.meta.env.VITE_API_BASE_URL;
+console.log("API_BASE =", API_BASE);
 
-export async function trackEvent(
-  businessSlug,
-  eventType,
-  metadata = {}
-) {
-
+export async function trackEvent(businessSlug, eventType, metadata = {}) {
   try {
-
     await axios.post(`${API_BASE}/analytics`, {
       business_slug: businessSlug,
       event_type: eventType,
-      metadata
+      metadata,
     });
-
   } catch (error) {
-
-    console.error(
-      error.response?.data || error.message
-    );
-
+    console.error(error.response?.data || error.message);
   }
 }
